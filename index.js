c397a451e021c65b5fbc0f7a38879321317bf5c3
Ketsia-a <iraketsia2001@gmail.com>
2020-10-21 07:01:52 +0200

    placed an alert

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-$('#myModal').on('shown.bs.modal', function() {
-    $('#myInput').trigger('focus')
-})
+function compute(form) {
+
+    if (form.size.value === "small") {
+        var size = parseInt(form.size.value, 1000)
+
+    } else if (form.size.value === "medium") {
+        var size = parseInt(form.size.value, 1500)
+    } else if (form.size.value === "large") {
+        var size = parseInt(form.size.value, 2000)
+    }
+
+    alert("your total order", size)
+}
