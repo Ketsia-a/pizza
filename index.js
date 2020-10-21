5d6be79ff3c13ca5e1af8d5285142e6b16dfe026
Ketsia-a <iraketsia2001@gmail.com>
2020-10-21 13:54:39 +0200

    made constructors created integers made functions for sum

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
+function order(size, crust, topping = []) {
+    this.crust = crust;
+    this.size = size;
+    this.name = topping;
+}
+
 function compute(form) {
 
-    if (form.size.value === "small") {
-        var size = parseInt(form.size.value, 1000)
+    var sizeprice = parseInt(document.getSelection('this.size'), value)
+    var crustprice = parseInt(document.getSelection('this.crust'), value)
 
-    } else if (form.size.value === "medium") {
-        var size = parseInt(form.size.value, 1500)
-    } else if (form.size.value === "large") {
-        var size = parseInt(form.size.value, 2000)
+    var toppingprice = []
+    var onep = 1000;
+    var twop = 500;
+    var threep = 1500;
+    var fourp = 700;
+    var fivep = 1200;
+
+    for (var i = 0; i < this.topping.length; y++) {
+        if (this.toppings[i] === "one") {
+            toppingprice.push(onep);
+        }
     }
+    for (i = 0; i < this.topping.length; y++) {
+        if (this.topping[i] === "two") {
+            toppingprice.push(twop);
+        }
+    }
+    for (i = 0; i < this.toppings.length; y++) {
+        if (this.topping[i] === "three") {
+            toppingprice.push(threep);
+        }
+    }
+    for (i = 0; i < this.topping.length; y++) {
+        if (this.topping[i] === "four") {
+            toppingprice.push(fourp);
+        }
+    }
+    for (i = 0; i < this.topping.length; y++) {
+        if (this.toppings[i] === "five") {
+            toppingprice.push(fivep);
+        }
+    }
+    var topsum = toppingprice.reduce(function(a, b) {
+        return a + b;
+    }, 0);
 
-    alert("your total order", size)
+    var Total = sizeprice + crustprice + topsum;
+
 }
